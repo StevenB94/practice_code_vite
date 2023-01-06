db8db686a47b81e9c90e422d6738591041a39d0b
StevenB94 <89290120+StevenB94@users.noreply.github.com>
2023-01-05 16:45:23 -0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App: React.FC = () => {
   return (
       <div className="flex flex-col justify-center items-center h-screen bg-black">
         <h1 className="text-white">HELLO, I'M A REACT DEVELOPER!</h1>
-        <p className="text-white">- Rohan Singh Rawat</p>
+        <p className="text-white">- Steven Ballerini</p>
       </div>
   )
 }
